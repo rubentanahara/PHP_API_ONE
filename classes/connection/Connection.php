5af6ad5ed1cdcbd6b387c6ffba4016cdd4ae0555
Ruben Tanahara <tanaharaguy@gmail.com>
2023-07-07 21:16:28 -0700

    Api actions part 1

--- classes/connection/Connection.php
+++ classes/connection/Connection.php
@@ -34,19 +34,53 @@ class Connection {
       if ($this->conn->connect_errno) {
         throw new Exception("Error en la conexión a la base de datos.");
       }
-      else {
-        echo "Connected!"
-      }
+     /* else {
+        echo "Connected";
+      }*/
+      
     } catch (Exception $e) {
       echo "Error: " . $e->getMessage();
       die();
     }
   }
-
+  //Iniciando conexión
   private function initConnection() {
     $address = dirname(__FILE__);
     $jsondata = file_get_contents($address . "/" . "config");
     return json_decode($jsondata, true);
+  }
+  // Convertir texto a UTF8 
+  private function ConvertToUTF8($arr){
+    array_walk_recursive($arr,function(&$item){
+      if(!mb_detect_encoding($item,'utf-8',true)) {
+          $item = utf_encode($item); 
+      }
+    });
+    return $arr;
+  }
+
+  // Acciones de la API
+  public function GetData($query){
+    $results = $this->conn->query($query);
+    $arr = array();
+    foreach ($results as $key) {
+      $arr[] = $key;
+    }
+    return $this->ConvertToUTF8($arr);
+  }
+
+  public function nonQuery($query) {
+    $results = $this->conn->query($query);
+    return $this->conn->affected_rows;
+  }
+
+  public function nonQueryId($query) {
+    $results = $this->conn->query($query);
+    $rows = $this->conn-> affected_rows;
+    if(rows>0){
+      return $this->conn->insert_id;
+    }
+    return 0;
   }
 }
 ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
   require_once "classes/connection/Connection.php";
   $conn = new Connection;
-
+  $query = "select * from pacientes";
+  print_r($conn->GetData($query));
 ?>
